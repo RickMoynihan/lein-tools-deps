9ec8ca17ef2fa85010af67bcf0bf58124e696c88
Rick Moynihan <rick.m@swirrl.com>
2017-12-14 01:44:05 +0000

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject lein-tools-deps "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Leiningen plugin to load :dependencies from deps.edn instead."
+  :url "https://github.com/RickMoynihan/lein-tools-deps"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/tools.deps.alpha "0.2.197-SNAPSHOT"]]
