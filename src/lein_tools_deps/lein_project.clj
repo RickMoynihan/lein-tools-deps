becfa7ed50b2e69e052b25076dc4635e3d9a0fb2
Hugh Aristotle Powell <hugh@swirrl.com>
2018-06-07 13:58:58 +0100

    Cache results from resolve-deps
    When a project has uses an S3 wagon the results are not cached. This
    leads to multiple out to the repository to get the same information.

--- src/lein_tools_deps/lein_project.clj
+++ src/lein_tools_deps/lein_project.clj
@@ -45,11 +45,13 @@
         (update :source-paths concat (vals classpath-overrides))
         (update :dependencies (partial remove (fn [[sym _version]] (contains? classpath-overrides sym)))))))
 
+(def ^:private tools-resolve-deps (memoize tools-deps/resolve-deps))
+
 (defn resolve-deps
   "Resolves all dependencies, meta merging them into the lein project map"
   [{:keys [root] {:keys [resolve-aliases aliases]} :lein-tools-deps/config :as project} deps]
   (let [args-map (tools-deps/combine-aliases deps (concat resolve-aliases aliases))
-        tdeps-map (tools-deps/resolve-deps deps args-map)]
+        tdeps-map (tools-resolve-deps deps args-map)]
     (-> project
         (update :dependencies concat (lein-dependencies tdeps-map))
         (update :source-paths concat (lein-source-paths root deps tdeps-map)))))
